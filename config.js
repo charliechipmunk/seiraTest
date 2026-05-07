8c6fa851dbf3aab84d9475e321d0b9b8ee152fef
charliechipmunk <55745044+charliechipmunk@users.noreply.github.com>
2026-05-07 22:19:44 +0200

    chat update
    Ora va anche su telefono e altre cose

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SB_URL: 'https://bkdkhdrmcwazfqmvgiki.supabase.co',
+    SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrZGtoZHJtY3dhemZxbXZnaWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzE4MjEsImV4cCI6MjA5Mzc0NzgyMX0.cuU7uAqy7jbCFSO60_oOTk6a9c2eq39lxpWGC4nHn5k' 
+};
