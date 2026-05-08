54c2e4c87568421a38aa23b0e4b8fcbdf90c5890
charliechipmunk <55745044+charliechipmunk@users.noreply.github.com>
2026-05-08 21:57:30 +0200

    password alla chat + altro

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
     SB_URL: 'https://bkdkhdrmcwazfqmvgiki.supabase.co',
-    SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrZGtoZHJtY3dhemZxbXZnaWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzE4MjEsImV4cCI6MjA5Mzc0NzgyMX0.cuU7uAqy7jbCFSO60_oOTk6a9c2eq39lxpWGC4nHn5k' 
+    SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrZGtoZHJtY3dhemZxbXZnaWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzE4MjEsImV4cCI6MjA5Mzc0NzgyMX0.cuU7uAqy7jbCFSO60_oOTk6a9c2eq39lxpWGC4nHn5k',
+    PASS_HASH: "139f1509cfb026187ffd40357e26ef1781b0ce2ac6a67ddef9a406032275b8d5"
 };
